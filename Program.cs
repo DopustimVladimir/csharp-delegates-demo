d5624a4410c8e99ad0714931bd70b49c8e364b4c
Vladimir <dopustimvladimir@gmail.com>
2021-12-26 20:53:06 +0600

    Move class President to it's own file

--- Program.cs
+++ Program.cs
@@ -14,29 +14,3 @@ sdpkPresidents.Sort(President.CompareByName);
 foreach (President p in sdpkPresidents) {
     Console.WriteLine(p.Name);
 }
-
-class President
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string LastName { get; set; }
-    public string Party { get; set; }
-
-    public President(int id, string name, string lastName, string party="")
-    {
-        Id = id;
-        Name = name;
-        LastName = lastName;
-        Party = party;
-    }
-
-    public static bool IsMemberOfTheSdpkParty(President p)
-    {
-        return p.Party == "SDPK";
-    }
-
-    public static int CompareByName(President p1, President p2)
-    {
-        return String.Compare(p1.Name, p2.Name);
-    }
-}
