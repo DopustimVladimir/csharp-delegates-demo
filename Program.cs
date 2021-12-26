ff426c441158c9199f095eb22374b3f419108acf
Vladimir <dopustimvladimir@gmail.com>
2021-12-26 18:12:25 +0600

    Restyle braces for methods

--- Program.cs
+++ Program.cs
@@ -22,18 +22,21 @@ class President
     public string LastName { get; set; }
     public string Party { get; set; }
 
-    public President(int id, string name, string lastName, string party="") {
+    public President(int id, string name, string lastName, string party="")
+    {
         Id = id;
         Name = name;
         LastName = lastName;
         Party = party;
     }
 
-    public static bool IsMemberOfTheSdpkParty(President p) {
+    public static bool IsMemberOfTheSdpkParty(President p)
+    {
         return p.Party == "SDPK";
     }
 
-    public static int CompareByName(President p1, President p2) {
+    public static int CompareByName(President p1, President p2)
+    {
         return String.Compare(p1.Name, p2.Name);
     }
 }
